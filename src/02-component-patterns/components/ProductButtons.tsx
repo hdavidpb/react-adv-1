53a0a2835a2c4fd426c9b977ce4fe8f7301d4c78
Hernan Plaza <hernan.plazabs@gmail.com>
2023-02-07 12:22:38 -0500

    [UPDATE]

--- src/02-component-patterns/components/ProductButtons.tsx
+++ src/02-component-patterns/components/ProductButtons.tsx
@@ -1,31 +1,38 @@
-import { useContext } from "react";
+import { useCallback, useContext } from "react";
 import { ProductContext } from "./ProductCard";
 
-import styles from '../styles/styles.module.css'
+import styles from "../styles/styles.module.css";
 
 export interface Props {
-    className?: string;
-    style?: React.CSSProperties 
+  className?: string;
+  style?: React.CSSProperties;
 }
 
 export const ProductButtons = ({ className, style }: Props) => {
+  const { increaseBy, counter, maxCount } = useContext(ProductContext);
 
-    const { increaseBy, counter } = useContext( ProductContext );
+  const isMaxReached = useCallback(() => {
+    return maxCount === counter;
+  }, [counter, maxCount]);
 
-    return (
-        <div 
-            className={ `${ styles.buttonsContainer} ${ className }` }
-            style={ style }
-        >
-            <button
-                className={ styles.buttonMinus }
-                onClick={ () => increaseBy( -1 ) }> - </button>
+  return (
+    <div className={`${styles.buttonsContainer} ${className}`} style={style}>
+      <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>
+        -
+      </button>
 
-            <div className={ styles.countLabel }> { counter } </div>
+      <div className={styles.countLabel}>{counter}</div>
 
-            <button
-                className={ styles.buttonAdd }
-                onClick={ () => increaseBy( +1 ) }> + </button>
-        </div>
-    );
-}
+      <button
+        className={
+          !isMaxReached()
+            ? styles.buttonAdd
+            : styles.buttonAdd + ` ${styles.disabled}`
+        }
+        onClick={() => increaseBy(+1)}
+      >
+        +
+      </button>
+    </div>
+  );
+};
--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,39 +1,52 @@
-import { useEffect, useRef, useState } from 'react'
-import { onChangeArgs, Product } from '../interfaces/interfaces';
-
+import { useEffect, useRef, useState } from "react";
+import { InitialValues, onChangeArgs, Product } from "../interfaces/interfaces";
 
 interface useProductArgs {
-    product: Product;
-    onChange?: ( args: onChangeArgs ) => void;
-    value?: number;
+  product: Product;
+  onChange?: (args: onChangeArgs) => void;
+  value?: number;
+  initialValues?: InitialValues;
 }
 
+export const useProduct = ({
+  onChange,
+  product,
+  value = 0,
+  initialValues,
+}: useProductArgs) => {
+  const [counter, setCounter] = useState<number>(initialValues?.count || value);
+  const isMounted = useRef(false);
 
-export const useProduct = ({ onChange, product, value = 0 }: useProductArgs) => {
+  const increaseBy = (value: number) => {
+    let newValue = Math.max(counter + value, 0);
 
-    const [ counter, setCounter ] = useState( value );
-
-    const isControlled = useRef( !!onChange )
-
-    const increaseBy = ( value: number ) => {
-
-        if( isControlled.current ) {
-            return onChange!({ count: value, product });
-        }
-        
-        const newValue = Math.max( counter + value, 0 )
-        setCounter( newValue );
-
-        onChange && onChange({ count: newValue, product });
+    if (initialValues?.maxCount) {
+      newValue = Math.min(newValue, initialValues.maxCount);
     }
 
-    useEffect(() => {
-        setCounter( value );
-    }, [ value ])
+    setCounter(newValue);
+    onChange && onChange({ count: newValue, product });
+  };
 
-    return {
-        counter,
-        increaseBy
-    }
+  useEffect(() => {
+    if (!isMounted.current) return;
+    setCounter(value);
+  }, [value]);
 
-}
+  useEffect(() => {
+    isMounted.current = true;
+  }, []);
+
+  const reset = () => {
+    setCounter(initialValues?.count || value);
+  };
+
+  return {
+    counter,
+    isMaxCountReached:
+      !!initialValues?.maxCount && counter === initialValues.maxCount,
+    maxCount: initialValues?.maxCount,
+    increaseBy,
+    reset,
+  };
+};
